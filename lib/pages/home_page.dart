9e21a486418918ad21704165c21e084ab7fd4e5d
tasakimiki <mi211327@edu.okinawa-ct.ac.jp>
2024-02-21 07:49:43 +0900

    aaa

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:speech_balloon/speech_balloon.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +8,87 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 170, 204, 255), 
       appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(40), child: AppBar()),
-      body: const SafeArea(child: Text('homeです')),
+          preferredSize: const Size.fromHeight(40), child: AppBar(
+          title: const Center(
+            child: Text(
+              'Navinator',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 120),
+
+                  const SpeechBalloon(
+                    nipLocation: NipLocation.bottom,
+                    borderColor: Color.fromARGB(255, 255, 255, 255),
+                    color: Colors.white,
+                    height: 50,
+                    width: 250,
+                    child: Center(
+                      child: Text(
+                        '遊びに行きますか？',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  Center(
+                    child: Image.asset(
+                      'char_images/char_imageA.png',
+                      width: 250,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  ElevatedButton.icon(
+                    icon: const Icon(
+                      Icons.touch_app,
+                      color: Colors.black,
+                    ),
+                    label: const Text(
+                      'ルートをシミュレーション',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+
+                        )
+                      ),
+                    style: ElevatedButton.styleFrom( 
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size.fromHeight(50),
+                    ),
+                    onPressed: () {},
+                  ), 
+                ]
+              )
+          
+
+            ],
+          ),
+
+        ),
+      ),
     );
   }
+}
+
+class RaisedButton {
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:feature_tasaki/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:yanbaru_hackathon/main.dart';
+// ignore: duplicate_import
+import 'package:feature_tasaki/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
