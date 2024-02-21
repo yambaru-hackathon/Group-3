5b0c4fc135c7fbfba51e38ecddc5f96e73d8310c
Mr_TKT <iketakkee@gmail.com>
2024-02-21 23:48:17 +0900

    selective chat routing little coded

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:speech_balloon/speech_balloon.dart';
 
+//homeページ
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -87,12 +88,14 @@ class HomePage extends StatelessWidget {
                         Icons.touch_app_outlined,
                         color: Colors.grey,
                       ),
-                      label: const Text('ルートをシミュレーション',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          )),
+                      label: const Text(
+                        'ルートをシミュレーション',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.white,
                         fixedSize: const Size.fromHeight(50),
@@ -103,7 +106,7 @@ class HomePage extends StatelessWidget {
                           PageRouteBuilder(
                             pageBuilder:
                                 (context, animation, secondaryAnimation) =>
-                                    SelectLocationPage(),
+                                    const SelectLocationPage(),
                             transitionDuration: const Duration(
                                 milliseconds: 0), // アニメーションの速度を0にする
                           ),
@@ -126,16 +129,13 @@ class HomePage extends StatelessWidget {
   }
 }
 
+//目的地入力
 class SelectLocationPage extends StatelessWidget {
-  SelectLocationPage({super.key});
-
-  // ignore: unused_field
-  final TextEditingController _textController = TextEditingController();
+  const SelectLocationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // resizeToAvoidBottomInset: false,
       backgroundColor: const Color(0xffaaccff),
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(40),
@@ -160,6 +160,7 @@ class SelectLocationPage extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -252,12 +253,23 @@ class SelectLocationPage extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  const Spacer(),
                   ElevatedButton(
                     onPressed: () {
                       Navigator.pop(context); // ホーム画面に戻る
                     },
-                    child: const Text('やめる'),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xffd32929),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0), // 縁を丸くする半径
+                        ),
+                        shadowColor: Colors.black),
+                    child: const Text(
+                      'やめる',
+                      style: TextStyle(color: Color(0xffffffff)),
+                    ),
                   ),
+                  const Spacer(),
                   ElevatedButton(
                     onPressed: () {
                       Navigator.push(
@@ -265,14 +277,25 @@ class SelectLocationPage extends StatelessWidget {
                         PageRouteBuilder(
                           pageBuilder:
                               (context, animation, secondaryAnimation) =>
-                                  const HomePage(),
+                                  const BeforeGoPage(),
                           transitionDuration: const Duration(
                               milliseconds: 0), // アニメーションの速度を0にする
                         ),
                       );
                     },
-                    child: const Text('次へ'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff1a69c6),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0), // 縁を丸くする半径
+                      ),
+                      shadowColor: Colors.black,
+                    ),
+                    child: const Text(
+                      '次へ',
+                      style: TextStyle(color: Color(0xffffffff)),
+                    ),
                   ),
+                  const Spacer(),
                 ],
               ),
               const SizedBox(
@@ -284,6 +307,366 @@ class SelectLocationPage extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+//目的地行くまでにしたいこと選択
+class BeforeGoPage extends StatefulWidget {
+  const BeforeGoPage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _BeforeGoPageState createState() => _BeforeGoPageState();
+}
+
+class _BeforeGoPageState extends State<BeforeGoPage> {
+  bool action1Checked = false;
+  bool action2Checked = false;
+  bool action3Checked = false;
+  bool action4Checked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffaaccff),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(40),
+        child: AppBar(
+          title: const Row(
+            children: [
+              SizedBox(
+                width: 72.4,
+              ),
+              Center(
+                child: Text(
+                  'Navinator',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 2,
+              color: Colors.grey,
+            ),
+            const SizedBox(height: 20),
+            const SpeechBalloon(
+              nipLocation: NipLocation.bottom,
+              borderColor: Color.fromARGB(255, 255, 255, 255),
+              color: Colors.white,
+              height: 50,
+              width: 250,
+              child: Center(
+                child: Text(
+                  '目的地までにしたいことは？',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Image.asset(
+                'lib/images/napi_guruguru.png',
+                height: 300,
+              ),
+            ),
+            // 目的地の入力フォームなどを配置
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xffc5e1ff),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      CheckboxListTile(
+                        title: const Text('食事'),
+                        value: action1Checked,
+                        onChanged: (value) {
+                          setState(() {
+                            action1Checked = value!;
+                          });
+                        },
+                      ),
+                      CheckboxListTile(
+                        title: const Text('景色を見る'),
+                        value: action2Checked,
+                        onChanged: (value) {
+                          setState(() {
+                            action2Checked = value!;
+                          });
+                        },
+                      ),
+                      CheckboxListTile(
+                        title: const Text('買い物'),
+                        value: action3Checked,
+                        onChanged: (value) {
+                          setState(() {
+                            action3Checked = value!;
+                          });
+                        },
+                      ),
+                      CheckboxListTile(
+                        title: const Text('ない'),
+                        value: action4Checked,
+                        onChanged: (value) {
+                          setState(() {
+                            action4Checked = value!;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context); // ホーム画面に戻る
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffd32929),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // 縁を丸くする半径
+                    ),
+                    shadowColor: Colors.black,
+                  ),
+                  child: const Text(
+                    'もどる',
+                    style: TextStyle(color: Color(0xffffffff)),
+                  ),
+                ),
+                const Spacer(),
+
+                //4択で選んだ時の分岐
+
+                ElevatedButton(
+                  onPressed: () {
+                    if (action1Checked) {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) =>
+                                  const SelectFoodPage(),
+                          transitionDuration: const Duration(
+                              milliseconds: 0), // アニメーションの速度を0にする
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff1a69c6),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // 縁を丸くする半径
+                    ),
+                    shadowColor: Colors.black,
+                  ),
+                  child: const Text(
+                    '次へ',
+                    style: TextStyle(color: Color(0xffffffff)),
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
+            const Spacer(),
+            Container(
+              height: 2.5,
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//ご飯の種類を選択
+class SelectFoodPage extends StatefulWidget {
+  const SelectFoodPage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SelectFoodPageState createState() => _SelectFoodPageState();
+}
+
+class _SelectFoodPageState extends State<SelectFoodPage> {
+  String selectedValue = 'お米';
+  List<String> dropdownItems = ['お米', '麺', 'パン', 'お肉', 'お寿司', 'ピザ'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffaaccff),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(40),
+        child: AppBar(
+          title: const Row(
+            children: [
+              SizedBox(
+                width: 72.4,
+              ),
+              Center(
+                child: Text(
+                  'Navinator',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 2,
+              color: Colors.grey,
+            ),
+            const SizedBox(height: 20),
+            const SpeechBalloon(
+              nipLocation: NipLocation.bottom,
+              borderColor: Color.fromARGB(255, 255, 255, 255),
+              color: Colors.white,
+              height: 50,
+              width: 250,
+              child: Center(
+                child: Text(
+                  'ご飯は何が食べたい？',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Image.asset(
+                'lib/images/napi_kirakira.png',
+                height: 300,
+              ),
+            ),
+
+            // 目的地の入力フォームなどを配置
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                width: 200,
+                decoration: BoxDecoration(
+                  color: const Color(0xffc5e1ff),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      DropdownButton<String>(
+                        value: selectedValue,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedValue = newValue!;
+                          });
+                        },
+                        items: dropdownItems
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context); // ホーム画面に戻る
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffd32929),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // 縁を丸くする半径
+                    ),
+                    shadowColor: Colors.black,
+                  ),
+                  child: const Text(
+                    'もどる',
+                    style: TextStyle(color: Color(0xffffffff)),
+                  ),
+                ),
+                const Spacer(),
+
+                //4択で選んだ時の分岐
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            const HomePage(),
+                        transitionDuration:
+                            const Duration(milliseconds: 0), // アニメーションの速度を0にする
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff1a69c6),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // 縁を丸くする半径
+                    ),
+                    shadowColor: Colors.black,
+                  ),
+                  child: const Text(
+                    '次へ',
+                    style: TextStyle(color: Color(0xffffffff)),
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
+            const Spacer(),
+            Container(
+              height: 2.5,
+              color: Colors.black,
+            ),
+          ],
         ),
       ),
     );
