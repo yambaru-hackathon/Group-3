5f80ff018c5ecddeb72ecc509b8f0acd08fbc8aa
tasakimiki <mi211327@edu.okinawa-ct.ac.jp>
2024-02-23 18:23:10 +0900

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2615,7 +2615,7 @@ class _ShowRoutePageState extends State<ShowRoutePage> {
                       shadowColor: Colors.black,
                     ),
                     child: const Text(
-                      'ホームa',
+                      'ホーム',
                       style: TextStyle(color: Color(0xffffffff)),
                     ),
                   ),
