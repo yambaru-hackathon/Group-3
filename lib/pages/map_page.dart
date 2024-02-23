484c27a18298a2844383ffaaee9f4f1728d603f7
takaesu11 <s.0711commandblock@gmail.com>
2024-02-23 09:47:07 +0900

    add map page

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+// ignore: library_prefixes
+import 'package:geocoding/geocoding.dart' as geoCoding;
+
+// ignore: unused_element
+Future<String?> _determinePosition() async{
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+  if(!serviceEnabled){
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+
+  if(permission == LocationPermission.denied){
+    permission = await Geolocator.requestPermission();
+    if(permission == LocationPermission.denied){
+      return Future.error('Location permissions are denied.');
+    }
+  }
+
+  if(permission == LocationPermission.deniedForever){
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.'
+    );
+  }
+
+  final position =  await Geolocator.getCurrentPosition();
+  final placeMarks = await geoCoding.placemarkFromCoordinates(position.latitude, position.longitude, localeIdentifier: "JP");
+  final placeMark = [placeMarks[0].country, placeMarks[0].administrativeArea, placeMarks[0].locality];
+  return placeMark[0];
+  }
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -46,7 +81,24 @@ class MapPage extends StatelessWidget {
           ),
         ),
       ),
-      body: const SafeArea(child: Text('mapです')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: InteractiveViewer(
+                minScale: 0.5,
+                maxScale: 2.0,
+                scaleEnabled: true,
+                panEnabled: true,
+                constrained: false,
+                child: Image.asset(
+                  'lib/images/map.png',
+                ),
+              )
+            ),
+          ],
+        )
+      )
     );
   }
 }
