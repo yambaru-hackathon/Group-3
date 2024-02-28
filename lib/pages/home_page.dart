8871a779d53d9bfd311fd4812c5887115d170fca
tasakimiki <mi211327@edu.okinawa-ct.ac.jp>
2024-02-28 09:18:47 +0900

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4064,7 +4064,7 @@ class _ShowRoutePageState extends State<ShowRoutePage> {
           ?.animateCamera(CameraUpdate.newLatLngBounds(bounds, 50));
     }
 
-    // MapData インスタンスを作成してリストに追加a
+    // MapData インスタンスを作成してリストに追加
     mapDataList.add(MapData(
       routeCoordinates: routeCoordinates,
       markers: markers,
